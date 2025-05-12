f1ade3db0f9ff5dc369b22ca6b11e70b54b0bc7c
Elizabeth <rxxivkey@gmail.com>
2025-05-12 14:18:30 +0300

    readme and corrections added

--- tests/test_category.py
+++ tests/test_category.py
@@ -4,7 +4,13 @@ from src.category import Category
 
 
 @pytest.fixture
-def category_init():
+def counting_everything():
+    Category.category_count = 0
+    Category.product_count = 0
+
+
+@pytest.fixture
+def category_init(counting_everything):
     return Category("Бытовая техника", "Качественные товары", ["Ноутбук", "Кофемашина"])
 
 
@@ -12,3 +18,8 @@ def test_category(category_init):
     assert category_init.name == "Бытовая техника"
     assert category_init.description == "Качественные товары"
     assert category_init.products == ["Ноутбук", "Кофемашина"]
+
+
+def test_counting(category_init):
+    assert Category.category_count == 1
+    assert Category.product_count == 2
