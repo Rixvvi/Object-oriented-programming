52b4aecb5259a09bc29c8f4314149e6c94664593
Elizabeth <rxxivkey@gmail.com>
2025-05-13 16:57:21 +0300

    working on new homework

--- src/product.py
+++ src/product.py
@@ -7,5 +7,24 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product: dict):
+        return cls(
+            product["name"],
+            product["price"],
+            product["description"],
+            product["quantity"]
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+        print('Цена не должна быть нулевая или отрицательная')
